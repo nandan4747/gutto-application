e10c9dc13c6cec10a27dadeb83ce6a6ec3bc1974
nandan4747 <nandankumar474747@gmail.com>
2026-08-19 17:22:55 +0530

    changed env key

--- src/api/API_DETAILS.ts
+++ src/api/API_DETAILS.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = import.meta.env.BACKEND_API || "http://localhost:10000";
+export const BASE_URL =
+  import.meta.env.VITE_BACKEND_API || "http://localhost:10000";
 //export const BASE_URL = "http://localhost:10000";
 export const API_DETAILS = {
   host: `${BASE_URL}/api`,
