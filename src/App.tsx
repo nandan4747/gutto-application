f0a976aa9c68caac2f81fdfd26b49d42a553673b
nandan4747 <nandankumar474747@gmail.com>
2026-08-19 11:56:49 +0530

    remove the socket provider context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Auth from "./pages/auth-pages/Auth";
-import { SocketProvider } from "../contexts/SocketProvider.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import Chat from "./pages/chat-page/Chat.tsx";
 import AppHeader from "./components/app_header/AppHeader.tsx";
@@ -10,27 +9,25 @@ import { ConversationProvider } from "../contexts/ConversationContext.tsx";
 function App() {
   return (
     <ConversationProvider>
-      <SocketProvider>
-        <div
-          style={{
-            display: "flex",
-            height: "100vh",
-            overflow: "hidden",
-          }}
-        >
-          <AppHeader />
+      <div
+        style={{
+          display: "flex",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
+        <AppHeader />
 
-          {/* The main chat area takes up the remaining space */}
-          <main style={{ flex: 1 }}>
-            <Routes>
-              <Route path="/auth" element={<Auth />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="/" element={<Chat />} />
-              </Route>
-            </Routes>
-          </main>
-        </div>
-      </SocketProvider>
+        {/* The main chat area takes up the remaining space */}
+        <main style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/auth" element={<Auth />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Chat />} />
+            </Route>
+          </Routes>
+        </main>
+      </div>
     </ConversationProvider>
   );
 }
