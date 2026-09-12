9dba0d2b16329dc0897b4ca9a52ce18740ad0a9d
nandan4747 <nandankumar474747@gmail.com>
2026-09-12 13:43:43 +0530

    added max height property

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.64bjglnheb8"
+    "revision": "0.hfn5v5dpmb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
