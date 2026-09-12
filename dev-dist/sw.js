3fa172ef6c2ff6028217f441108823f72585f838
nandan4747 <nandankumar474747@gmail.com>
2026-09-12 13:40:45 +0530

    color changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.1edm215vit8"
+    "revision": "0.64bjglnheb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
