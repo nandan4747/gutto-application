f6a2dfdf3bb0074515a3f452b914b94c1ad33088
nandan4747 <nandankumar474747@gmail.com>
2026-09-13 20:09:50 +0530

    changed the light theme primary color

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.231c0jk2fj8"
+    "revision": "0.g817mn9paeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
