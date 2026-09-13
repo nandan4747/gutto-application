f68ac008f1e3e793f08f94aed2669c310ce4311e
nandan4747 <nandankumar474747@gmail.com>
2026-09-13 20:05:41 +0530

    fixed the actual commit one

--- src/api/API_DETAILS.ts
+++ src/api/API_DETAILS.ts
@@ -1,6 +1,6 @@
-//export const BASE_URL =
-import.meta.env.VITE_BACKEND_API || "http://localhost:10000";
-export const BASE_URL = "http://localhost:10000";
+export const BASE_URL =
+  import.meta.env.VITE_BACKEND_API || "http://localhost:10000";
+//export const BASE_URL = "http://localhost:10000";
 export const API_DETAILS = {
   host: `${BASE_URL}/api`,
 };
