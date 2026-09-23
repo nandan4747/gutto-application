5b6dc5d4be0fdcf7dc47d2d7e77de51683da2b70
nandan4747 <nandankumar474747@gmail.com>
2026-09-23 13:34:33 +0530

    FIX : friend request

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.r2lma375sc"
+    "revision": "0.460n7vkbim"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
